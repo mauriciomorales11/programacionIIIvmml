e21bc1a8467ed0694556a7a02d39fc7c00354f1d
mauriciomorales11 <victormauriciomoraleslopez7@gmail.com>
2020-03-23 11:08:19 -0400

    tareas 5

---
+++ tareas/tarea 5/tarea 5/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_5
+{
+    class Program
+    {
+        int opc;
+        int[] cola = new int[max];
+        int final = -1;
+        int frente = -1;
+        public static int max = 10;
+        public void Menu()
+        {
+            Console.WriteLine(" opciones: ");
+            Console.WriteLine(" 1.Agregar por el frente  \n 2.agregar por el final  \n 3.eliminar por el frente  \n 4.eliminar por el final \n 5.imprimir elementos \n 6.salir");
+            opc = int.Parse(Console.ReadLine());
+            switch (opc)
+            {
+                case 1: agregar_por_el_frente(); break;
+                case 2: agregar_por_el_final(); break;
+                case 3: eliminar_por_el_frente(); break;
+                case 4: eliminar_por_el_final(); break;
+                case 5: imprimir_elementos(); break;
+                case 6: Console.WriteLine("Sesion finalizada"); break;
+                default: Console.WriteLine("Seleccione una opcion del menu"); break;
+            }
+        }
+        public Boolean vacia()
+        {
+            if (frente == -1)
+                return true;
+            else
+                return false;
+        }
+        public Boolean llena()
+        {
+            if (frente == 0 && final == max - 1)
+                return true;
+            else
+                return false;
+        }
+        public void agregar_por_el_frente()
+        {
+            if (llena() == true)
+                Console.WriteLine("la cola esta llena");
+            else
+                Console.WriteLine("agregar dato");
+            if (frente == 0)
+            {
+                Console.WriteLine("no se puede agregar por el frente");
+            }
+            if (frente > 0)
+            {
+                frente--;
+                cola[frente] = int.Parse(Console.ReadLine());
+            }
+            if (frente == -1)
+            {
+                frente++;
+                cola[frente] = int.Parse(Console.ReadLine());
+                if (frente == 0)
+                { final = 0; }
+            }
+        }
+        public void agregar_por_el_final()
+        {
+            if (llena() == true)
+                Console.WriteLine("la cola esta llena");
+            else
+                Console.WriteLine("agregar dato");
+            if (final < max - 1)
+            {
+                final++;
+                cola[final] = int.Parse(Console.ReadLine());
+            }
+            if (final == max - 1)
+            {
+                Console.WriteLine("no se puede agregar por el final");
+            }
+            if (final == -1)
+            {
+                final++;
+                cola[final] = int.Parse(Console.ReadLine());
+                if (final == 0)
+                    frente = 0;
+            }
+
+        }
+        public void eliminar_por_el_frente()
+        {
+            if (vacia() == true)
+                Console.WriteLine("La cola esta vacia");
+            else
+                Console.WriteLine("Se eliminara el dato por el frente" + cola[frente]);
+            frente++;
+        }
+        public void eliminar_por_el_final()
+        {
+            if (vacia() == true)
+                Console.WriteLine("La cola esta vacia");
+            else
+                Console.WriteLine("Se eliminara el dato por el final" + cola[final]);
+            final--;
+        }
+        public void imprimir_elementos()
+        {
+            if (vacia() == true)
+                Console.WriteLine("Cola vacia");
+            else
+                for (int i = frente; i <= final; i++)
+                    Console.Write(" " + cola[i] + '\t');
+            Console.WriteLine("  ");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            do
+            {
+                p.Menu();
+            } while (p.opc != 6);
+        }
+    }
+}
