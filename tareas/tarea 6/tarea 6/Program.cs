1a861151b9d929512c187ccd579ca07ab819117b
mauriciomorales11 <victormauriciomoraleslopez7@gmail.com>
2020-03-24 08:28:06 -0400

    tareas 6

---
+++ tareas/tarea 6/tarea 6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar el numero: ");
+            int A = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= A; i++)
+            {
+                Console.WriteLine("fibonacci: " + (i + 1) + FactorialRecursivo(i));
+            }
+            Console.ReadKey();
+        }
+        static int FactorialRecursivo(int A)
+        {
+            if (A == 0 || A == 1)
+            {
+                return 1;
+            }    
+            else
+            {
+                return A * FactorialRecursivo(A - 1) + A * FactorialRecursivo(A - 2);
+            }          
+        }
+
+
+    }
+
+}
