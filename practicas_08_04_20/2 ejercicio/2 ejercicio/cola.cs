3a77e37bd42902bfca7e69341cae3200b09702cc
mauriciomorales11 <victormauriciomoraleslopez7@gmail.com>
2020-04-08 22:58:48 -0400

    practicas_08_04_20

---
+++ practicas_08_04_20/2 ejercicio/2 ejercicio/cola.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_ejercicio
+{
+    class Cola<T>
+    {
+        public const int Maximo = 10;
+        private T[] arreglo = new T[Maximo];
+        private int final = -1;
+        private int frente = 0;
+
+        public void Insertar(T valor)
+        {
+            if (!LLena())
+            {
+                final++;
+                arreglo[final] = valor;
+            }
+            else
+                Console.WriteLine("La Cola esta llena");
+        }
+        public T Eliminar()
+        {
+            if (!Vacia())
+            {
+                frente++;
+                return arreglo[frente - 1];
+            }
+            else
+            {
+                Console.WriteLine("La Cola esta Vacia");
+                return arreglo[frente];
+            }
+        }
+        public void mostrar()
+        {
+            for (int i = frente; i <= final; i++)
+                Console.WriteLine(arreglo[i] + " ");
+
+        }
+        private Boolean Vacia()
+        {
+            return final < frente;
+        }
+        private Boolean LLena()
+        {
+            return final == Maximo;
+        }
+
+    }
+}
---
+++ practicas_08_04_20/3 ejercicio/3 ejercicio/infectados.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_ejercicio
+{
+    class infectado
+    {
+        public infectado(string nombre, string primerapellido, string segundoapellido, string estado, string sexo, int edad)
+        {
+            this.nombre = nombre;
+            this.primerapellido = primerapellido;
+            this.segundoapellido = segundoapellido;
+            this.estado = estado;
+            this.sexo = sexo;
+            this.edad = edad;
+        }
+
+        public String nombre { get; set; }
+        public String primerapellido { get; set; }
+        public String segundoapellido { get; set; }
+        public String estado { get; set; }
+        public String sexo { get; set; }
+
+        public int edad { get; set; }
+
+        override public string ToString()
+        {
+            return nombre + " " + primerapellido + " " + segundoapellido + " " + edad.ToString() + "     " + sexo + "     " + estado;
+        }
+
+
+
+    }
+}
