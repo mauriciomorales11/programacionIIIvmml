7f1d5956ad8272e3b6ed623aeab5623316c06f92
mauriciomorales11 <victormauriciomoraleslopez7@gmail.com>
2020-04-26 23:02:21 -0400

    prototipe.vm

---
+++ tareas/prototipe/prototipe/celular.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototipe
+{
+    class Celular
+    {
+        Marca marca;
+        string Modelo;
+        pantallatamaño pantallatamaño;
+        Color color;
+        int memoria;
+        procesador procesador;
+        public Celular(Marca marca, string modelo, pantallatamaño pantallatamaño, Color color, int memoria, procesador procesador)
+        {
+            this.marca = marca;
+            Modelo = modelo;
+            this.pantallatamaño = pantallatamaño;
+            this.color = color;
+            this.memoria = memoria;
+            this.procesador = procesador;
+        }
+        public override string ToString()
+        {
+            return marca + Modelo + pantallatamaño + color + memoria + procesador;
+        }
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+    }
+    enum Marca
+    {
+        apple, samsung, htc
+    }
+    enum Color
+    {
+        Rojo, Negro, Plateado
+    }
+}
---
+++ tareas/prototipe/prototipe/procesador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototipe
+{
+    class procesador
+    {
+        String Marca;
+        int CantidadNucleos;
+        double Frecuencia;
+
+        public procesador(string marca, int cantidadNucleos, double frecuencia)
+        {
+            Marca = marca;
+            CantidadNucleos = cantidadNucleos;
+            Frecuencia = frecuencia;
+        }
+
+        public override string ToString()
+        {
+            return Marca + CantidadNucleos.ToString() + Frecuencia.ToString();
+        }
+    }
+}
